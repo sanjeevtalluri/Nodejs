233b18be14d8ce1b66b41779c9205e21c191cb4c
sanjeevtalluri <sanjeevtalluri@gmail.com>
2023-03-30 00:45:29 +0530

    Lets Revise Javascript concepts

--- Basics/demo.js
+++ Basics/demo.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+const product = (a,b) => a*b;
+console.log(product(2,3));
+const student = {
+    name : 'testName',
+    age : 20,
+    display(){
+        console.log("name is : "+this.name+" age is : "+this.age);
+    }
+}
+
+student.display();
