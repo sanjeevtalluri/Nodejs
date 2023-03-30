5a3ef5f045f749bd7e3a09f2849bc54fa7517b4b
sanjeevtalluri <sanjeevtalluri@gmail.com>
2023-03-30 21:44:54 +0530

    Understanding the basics

--- Basics/demo.js
+++ Basics/demo.js
@@ -1,11 +1,5 @@
-const product = (a,b) => a*b;
-console.log(product(2,3));
-const student = {
-    name : 'testName',
-    age : 20,
-    display(){
-        console.log("name is : "+this.name+" age is : "+this.age);
-    }
-}
-
-student.display();
+const http = require('http');
+const server = http.createServer((req,res)=>{
+    console.log('sanjeev');
+})
+server.listen(4000);
