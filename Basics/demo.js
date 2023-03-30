c2c230772f48b0513d018e013c89736577c6430d
sanjeevtalluri <sanjeevtalluri@gmail.com>
2023-03-30 22:28:23 +0530

    Request / Response

--- Basics/demo.js
+++ Basics/demo.js
@@ -1,5 +1,20 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
-    console.log('sanjeev');
+    let body = '<body><h1>Welcome to Default</h1></body>';
+    
+    if(req.url === '/home'){
+        body = '<body><h1>Welcome to Home</h1></body>'
+    }
+    else if(req.url === '/about'){
+        body = '<body><h1>Welcome to About Us page</h1></body>'
+    }
+    else if(req.url === '/node'){
+        body = '<body><h1>Welcome to my Node Js project</h1></body>';
+    }
+    res.write('<html>');
+    res.write('<head><title>Demo</title></head>');
+    res.write(body);
+    res.write('</html>');
+    res.end();
 })
 server.listen(4000);
