fa762dd82267f6b4404831b3e133d3463eab9781
sanjeevtalluri <sanjeevtalluri@gmail.com>
2023-03-31 00:21:39 +0530

    Writing and reading from a file

--- Basics/demo.js
+++ Basics/demo.js
@@ -1,20 +1,41 @@
 const http = require('http');
-const server = http.createServer((req,res)=>{
-    let body = '<body><h1>Welcome to Default</h1></body>';
-    
-    if(req.url === '/home'){
-        body = '<body><h1>Welcome to Home</h1></body>'
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+    const method = req.method;
+    if (req.url === '/') {
+        if (fs.existsSync('sample.txt')) {
+            fs.readFile('sample.txt', 'utf8', (err, data) => {
+                res.write('<html>');
+                res.write('<head><title>Demo</title></head>');
+                res.write('<body><h1>' + data + '</h1><form action = "/message" method = "post"><input type= "text" name = "message"><button type="submit">submit</button></form></body>');
+                res.write('</html>');
+                return res.end();
+            })
+        }
+        else {
+            res.write('<html>');
+            res.write('<head><title>Demo</title></head>');
+            res.write('<body><form action = "/message" method = "post"><input type= "text" name = "message"><button type="submit">submit</button></form></body>');
+            res.write('</html>');
+            return res.end();
+        }
+
+
     }
-    else if(req.url === '/about'){
-        body = '<body><h1>Welcome to About Us page</h1></body>'
+    else if (method === 'POST' && req.url === '/message') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        })
+        req.on('end', () => {
+            const bodyParsed = Buffer.concat(body).toString();
+            const inputValue = bodyParsed.split('=')[1];
+            fs.writeFile('sample.txt', inputValue, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            })
+        })
     }
-    else if(req.url === '/node'){
-        body = '<body><h1>Welcome to my Node Js project</h1></body>';
-    }
-    res.write('<html>');
-    res.write('<head><title>Demo</title></head>');
-    res.write(body);
-    res.write('</html>');
-    res.end();
 })
 server.listen(4000);
