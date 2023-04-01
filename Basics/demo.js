1c4698cbb087c7b5d682c1acbedd1e4b8759852a
sanjeevtalluri <sanjeevtalluri@gmail.com>
2023-04-01 11:43:02 +0530

    Lets get started with Express

--- Basics/demo.js
+++ Basics/demo.js
@@ -1,5 +1,20 @@
-const http = require('http');
-const route = require('./route');
+//const http = require('http');
+//const route = require('./route');
 
-const server = http.createServer(route);
-server.listen(3000);
+const express = require('express');
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log("in middleware 1");
+    next();
+})
+
+app.use((req,res,next)=>{
+    console.log("in middleware 2");
+    res.send("<h1>This is express js</h1>")
+})
+
+app.listen(3000);
+
+//const server = http.createServer(route);
+//server.listen(3000);
