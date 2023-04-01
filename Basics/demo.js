e7bcad1c747bde7acac197fa273a31ba2836dbc3
sanjeevtalluri <sanjeevtalluri@gmail.com>
2023-04-01 09:00:03 +0530

    Clean Up Code

--- Basics/demo.js
+++ Basics/demo.js
@@ -1,41 +1,5 @@
 const http = require('http');
-const fs = require('fs');
-const server = http.createServer((req, res) => {
-    const method = req.method;
-    if (req.url === '/') {
-        if (fs.existsSync('sample.txt')) {
-            fs.readFile('sample.txt', 'utf8', (err, data) => {
-                res.write('<html>');
-                res.write('<head><title>Demo</title></head>');
-                res.write('<body><h1>' + data + '</h1><form action = "/message" method = "post"><input type= "text" name = "message"><button type="submit">submit</button></form></body>');
-                res.write('</html>');
-                return res.end();
-            })
-        }
-        else {
-            res.write('<html>');
-            res.write('<head><title>Demo</title></head>');
-            res.write('<body><form action = "/message" method = "post"><input type= "text" name = "message"><button type="submit">submit</button></form></body>');
-            res.write('</html>');
-            return res.end();
-        }
+const route = require('./route');
 
-
-    }
-    else if (method === 'POST' && req.url === '/message') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        })
-        req.on('end', () => {
-            const bodyParsed = Buffer.concat(body).toString();
-            const inputValue = bodyParsed.split('=')[1];
-            fs.writeFile('sample.txt', inputValue, (err) => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            })
-        })
-    }
-})
-server.listen(4000);
+const server = http.createServer(route);
+server.listen(3000);
---
+++ Basics/route.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+const requestHandler = (req,res)=>{
+    console.log('in');
+    const method = req.method;
+    const url = req.url;
+    if (url === '/') {
+        if (fs.existsSync('sample.txt')) {
+            fs.readFile('sample.txt', 'utf8', (err, data) => {
+                res.write('<html>');
+                res.write('<head><title>Demo</title></head>');
+                res.write('<body><h1>' + data + '</h1><form action = "/message" method = "post"><input type= "text" name = "message"><button type="submit">submit</button></form></body>');
+                res.write('</html>');
+                return res.end();
+            })
+        }
+        else {
+            res.write('<html>');
+            res.write('<head><title>Demo</title></head>');
+            res.write('<body><form action = "/message" method = "post"><input type= "text" name = "message"><button type="submit">submit</button></form></body>');
+            res.write('</html>');
+            return res.end();
+        }
+
+
+    }
+    else if (method === 'POST' && url === '/message') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        })
+        req.on('end', () => {
+            const bodyParsed = Buffer.concat(body).toString();
+            const inputValue = bodyParsed.split('=')[1];
+            fs.writeFile('sample.txt', inputValue, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            })
+        })
+    }
+    
+}
+
+module.exports = requestHandler;
